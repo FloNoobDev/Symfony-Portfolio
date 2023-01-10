cbf3b86ec50da2256ab220567bf59f0ba758c174
FloNoobDev <77100958+FloNoobDev@users.noreply.github.com>
2023-01-10 18:26:21 +0100

    WIP Admin

--- src/Controller/AdminProfileController.php
+++ src/Controller/AdminProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProfilRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,9 +10,24 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminProfileController extends AbstractController
 {
     #[Route('/admin/profile', name: 'admin_profile')]
-    public function index(): Response
+    public function index(ProfilRepository $profilRepository): Response
     {
         return $this->render('admin/admin_profile/index.html.twig', [
+            'profiles'=>$profilRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/admin/profile/create', name: 'create_profile')]
+    public function create(): Response
+    {
+        return $this->render('admin/_profile/createProfile.html.twig', [
+            'controller_name' => 'AdminProfileController',
+        ]);
+    }
+    #[Route('/admin/profile/{id}', name: 'update_profile')]
+    public function update(): Response
+    {
+        return $this->render('admin/_profile/updateProfile.html.twig', [
             'controller_name' => 'AdminProfileController',
         ]);
     }
