db145fde12506d82cad2ee84dc9e686b691edb26
FloNoobDev <77100958+FloNoobDev@users.noreply.github.com>
2023-01-10 16:15:00 +0100

    WIP

--- src/Controller/PageHomeController.php
+++ src/Controller/PageHomeController.php
@@ -2,19 +2,26 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Contact;
+use App\Form\ContactType;
+use App\Repository\SkillRepository;
+use App\Repository\ProfilRepository;
+use App\Repository\ContactRepository;
+use App\Repository\SkillCategoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PageHomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ProfilRepository $profilRepository, SkillCategoryRepository $skillCategoryRepository, SkillRepository $skillRepository, ContactRepository $contactRepository): Response
     {
         return $this->render('home/index.html.twig', [
-            'skills' => '',
-            'production' => '',
-            'formContact' => ''
+            'skillsCategories' => $skillCategoryRepository->findAll(),
+            // 'production' => '',
+            'formContact' => $this->createForm(ContactType::class, new Contact()),
+            'profil' => $profilRepository->find(1)
         ]);
     }
 }
